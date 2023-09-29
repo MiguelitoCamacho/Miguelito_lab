cfcd7505b295b8723de988767c790c74bab95e5c
MiguelitoCamacho <miguel.camacho.second@gmail.com>
2023-09-29 14:17:47 +0200

    training with frank

---
+++ python/test.py
@@ -0,0 +1,11 @@
+def analyze_scRNA(data):
+    # Your code here
+    pass
+
+def main():
+    # Your code here
+    data = load_data()
+    analyze_scRNA(data)
+
+if __name__ == "__main__":
+    main()
