fa0ca445dd158c8380e207cdbc1c38f2f768e423
MiguelitoCamacho <miguel.camacho.second@gmail.com>
2023-09-29 14:36:43 +0200

    Counting A in a small DNA sequence

--- python/test.py
+++ python/test.py
@@ -1,11 +1,18 @@
 def analyze_scRNA(data):
     # Your code here
-    pass
+     count = data.count("A")
+     return count
+
+def load_data():
+    # Your code here
+    data = "ATCG" * 5
+    return data
 
 def main():
     # Your code here
     data = load_data()
-    analyze_scRNA(data)
+    result = analyze_scRNA(data)
+    print(result)
 
 if __name__ == "__main__":
     main()
